6fcd1b3ca3a09a848ce845474426149c16c768e3
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2021-01-15 01:05:22 +0000

    Add domain util test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,16 +4,16 @@ import sinon from 'sinon'
 import * as domain from '../src/utils/domain'
 import app from '../src/index'
 
-describe('GET /', () => {
-  const sandbox = sinon.createSandbox()
+const sandbox = sinon.createSandbox()
 
+describe('GET /', () => {
   afterEach(() => {
-    sandbox.reset()
+    sandbox.restore()
   })
-  
+
   it('should respond with 200 and valid headers', async () => {
     const res = await request(app).get('/')
-      
+
     expect(res.status).to.equal(200)
     expect(res.headers['status']).to.equal('200 OK')
     expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
---
+++ test/utils/domain.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+import sinon from 'sinon'
+import config from 'config'
+import * as domain from '../../src/utils/domain'
+
+const sandbox = sinon.createSandbox()
+
+describe('domain util', () => {
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+  
+  it('should return a domain based on config values', () => {
+    const s = sandbox.stub(config, 'get');
+    s.withArgs('domain').returns('fake-domain')
+    s.withArgs('port').returns('0000')
+
+    expect(domain.getEnvBasedDomain()).to.equal('fake-domain:0000')
+  })
+})
