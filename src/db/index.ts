183f3037d260e4c21edef2dcae8e3d78b66d578b
David Chubabriya <31448563+davidc6@users.noreply.github.com>
2021-01-26 12:47:54 +0000

    Update pg connection method

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
 import { Pool, QueryResult } from "pg"
-const pool = new Pool({ query_timeout: 3000 })
+import dotenv from "dotenv"
+dotenv.config()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  query_timeout: 3000,
+})
 
 export const query = async (text: string, params?: string[]): Promise<QueryResult> => {
   return pool.query(text, params)
